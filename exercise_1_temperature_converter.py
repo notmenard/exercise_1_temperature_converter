737ce723340bb1672a10d17c7c7fba8b1fe73a67
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 11:47:55 +0800

    Asking for user's second input

--- exercise_1_temperature_converter.py
+++ exercise_1_temperature_converter.py
@@ -3,3 +3,10 @@ print(' Temperature Converter '.center(50, "-"))
 
 # Ask the user to input a temperature.
 temperature = float(input("\nEnter the temperature: "))
+
+# Ask the user to select the conversion type.
+print("\nConversions:")
+print("0 for Celsius to Fahrenheit")
+print("1 for Fahrenheit to Celsius")
+
+conversion = input("\nSelect a conversion type: ")
