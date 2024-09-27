0e52f1105b5481fb8af9750ddb1954e231f445c7
Menard Sabugo <menardsabugo@gmail.com>
2024-09-27 11:44:23 +0800

    Asking for user's input

--- exercise_1_temperature_converter.py
+++ exercise_1_temperature_converter.py
@@ -1,2 +1,5 @@
 # Displaying the introduction line
 print(' Temperature Converter '.center(50, "-"))
+
+# Ask the user to input a temperature.
+temperature = float(input("\nEnter the temperature: "))
